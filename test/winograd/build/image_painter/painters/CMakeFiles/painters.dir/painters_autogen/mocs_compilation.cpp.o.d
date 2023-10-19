5e68857201e007529a3692124fa6d62382b7a0ba
Alexey <zam7@icloud.com>
2023-10-19 12:23:36 +0200

    clear

--- test/winograd/build/image_painter/painters/CMakeFiles/painters.dir/painters_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-image_painter/painters/CMakeFiles/painters.dir/painters_autogen/mocs_compilation.cpp.o: \
- /home/alexey/projects/linear_algebra/test/winograd/build/image_painter/painters/painters_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
